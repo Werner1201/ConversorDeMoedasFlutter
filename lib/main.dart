7a731c7a2e1dd93eb629b519fbd08ff6d0f37c88
Werner1201 <werner.romling@unigranrio.br>
2019-10-22 11:19:29 -0300

    Construindo o Layout e Trabalhando com o Future para armazenar infos

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=cde39249";
 
 void main() async{
-
-  http.Response response = await http.get(request);
-
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
-
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
 }
+
+//Funcao que pega os dados Da API de maneira Assincrona
+//Decodifica o JSON no corpo da resposta em um Map.
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: ,
+    );
+  }
+}
+
+
+
